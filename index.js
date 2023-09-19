5392c7894172112a347bc29c2b158468a75ffbae
Leanderson Lima <leanderson.devlima@gmail.com>
2023-09-19 20:34:58 -0300

    Cliente1_2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const nomeCliente1 = "Juliana";
+const idadeCliente1 = 20;
+const membroCliente1 = true;
+
+console.log("Cliente 1:");
+console.log("Nome: " + nomeCliente1);
+console.log("Idade: " + idadeCliente1);
+console.log("Status de Membro: " + membroCliente1);
+
+const nomeCliente2 = "Leanderson";
+const idadeCliente2 = 25;
+const membroCliente2 = false;
+
+console.log("\nCliente 2:");
+console.log("Nome: " + nomeCliente2);
+console.log("Idade: " + idadeCliente2);
+console.log("Status de Membro: " + membroCliente2);
