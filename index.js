a956bf92743fdbc4ca34e614594067ed9d3e79b6
Inacio Sousa <franciscojosesousas@gmail.com>
2023-09-27 13:48:30 -0300

    Update Branch

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+//crie três constantes: nomeCliente, idadeCliente e eMembro para armazenar informações sobre um cliente específico.
+const nomeCliente = "Inacio";
+const idadeCliente = 32;
+const eMembro = true;
 
